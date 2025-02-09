a6a77369abcb4780a8733e18e8c93e0c836a3c1a
Siddhant Pandey <siddhantpandey1305@gmail.com>
2025-02-09 23:59:46 +0530

    historyFeature

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import "../App.css";
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, Route, useNavigate } from 'react-router-dom'
 
 export default function LandingPage() {
+
+    const route = useNavigate();
+
     return ( 
         <div className="landingPageContainer">
             <nav>
@@ -10,9 +13,15 @@ export default function LandingPage() {
                     <h2>View Stream</h2>
                 </div>
                 <div className='navList'>
-                    <p>Join as Guest</p>
-                    <p>Register</p>
-                    <div role='button'>
+                    <p onClick={() => {
+                        router("/guest123")
+                    }}>Join as Guest</p>
+                    <p onClick={() => {
+                        router("/auth")
+                    }}>Register</p>
+                    <div onClick={() => {
+                        router("/auth")
+                    }} role='button'>
                         <p>Login</p>
                     </div>
                 </div>
